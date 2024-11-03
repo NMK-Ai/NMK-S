bb3f78fb85ec4a244693bde1b6285b2b53bd74ab
DevTekVE <devtekve@gmail.com>
2024-11-03 07:58:51 +0100

    Refactor Hyundai ESCC code structure
    Moved ESCC-related functions to opendbc directory for better organization. This change consolidates ESCC logic within the main Hyundai CAN directory, improving code maintainability and readability.

--- sunnypilot/car/hyundai/escc.py
+++
@@ -1,5 +0,0 @@
-def update_escc_values(scc12_values, CS):
-    scc12_values["AEB_CmdAct"] = CS.escc_cmd_act
-    scc12_values["CF_VSM_Warn"] = CS.escc_aeb_warning
-    scc12_values["CF_VSM_DecCmdAct"] = CS.escc_aeb_dec_cmd_act
-    scc12_values["CR_VSM_DecCmd"] = CS.escc_aeb_dec_cmd
